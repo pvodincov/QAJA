86383703c79d7bbf22be3c10208266b396d6095d
p-odintcov <pvodincov@yandex.ru>
2022-07-01 11:40:56 +0300

    module30Final

---
+++ Modul30Final/src/test/java/PersonTest.java
@@ -0,0 +1,30 @@
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class PersonTest {
+
+    @DataProvider(name = "isTeenagerTestAge")
+    Object[][] sumDataProvider() {
+        return new Object[][]{
+                {-1, false},
+                {0, false},
+                {13, true},
+                {15, true},
+                {19, true},
+                {30, false}
+        };
+
+    }
+        @Test (dataProvider = "isTeenagerTestAge")
+
+        public void isTeenagerTest (int arg1, boolean arg2){
+            boolean result = Person.isTeenager(arg1);
+            System.out.println(result);
+            assertEquals (result,arg2);
+
+        }
+    }
+
